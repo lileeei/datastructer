5d94405686f0dc922811c9197f1f07bbac90bd12
lileeei <lileiwell1993@163.com>
2016-12-18 22:06:02 +0800

    Update skiplist.go

--- skiplist.go
+++ skiplist.go
@@ -1,30 +1,83 @@
 package skiplist
 
+import (
+	"math/rand"
+	"time"
+)
+
+const (
+	SKIPLIST_MAXLEVEL = 32
+	SKIPLIST_P        = 0.25
+)
+
+type CompareFunc func(a *SkipListNode, b *SkipListNode) bool
+
 //SkipListLevel ...
 type SkipListLevel struct {
 	levelForward *SkipListNode
-	span uint
+	levelBack    *SkipListNode
+	span         uint
 }
 
 //SkipListNode ...
 type SkipListNode struct {
-	Data inteface{}
+	Key     interface{}
+	Data    interface{}
 	Forward *SkipListNode
-	level []SkipListLevel
+	level   []SkipListLevel
 }
 
 //SkipList ...
 type SkipList struct {
-	head *SkipListNode
-	tail *SkipListNode
-	Level uint
-	length uint
+	head    *SkipListNode
+	tail    *SkipListNode
+	Level   uint
+	length  uint
 	Compare func(a *SkipListNode, b *SkipListNode) bool
 }
 
 //-----------------------------operation for skiplist
+func CreateSkipList() *SkipList {
+	skiplist := new(SkipList)
 
+	return skiplist
+}
 
+func (skiplist *SkipList) SetCompareFunc(compare CompareFunc) {
+	skiplist.Compare = compare
+}
+
+func (skiplist *SkipList) InsertNode(node *SkipListNode) {
+	//
+}
 
 //-----------------------------operation for skipnode
+/*
+ *todo
+ *
+**/
 
+func createSkipNode(key interface{}, data interface{}, level uint) *SkipListNode {
+	node := new(SkipListNode)
+	node.Key = key
+	node.Data = data
+	node.level = make([]SkipListLevel, level)
+
+	return node
+}
+
+//-----------------------------common function
+func randLevel() uint {
+	var level = 1
+	rand.Seed(time.Now().Unix())
+	for (rand.Int() & 0xffff) < (SKIPLIST_P * 0xffff) {
+		level++
+	}
+
+	if level < SKIPLIST_MAXLEVEL {
+
+		return level
+	}
+
+	return (level & SKIPLIST_MAXLEVEL)
+}
