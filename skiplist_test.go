8c4c747703043cfe880318080b039aacd40276a8
lileeei <lileiwell1993@163.com>
2016-12-23 14:41:14 +0800

    Update skiplist_test.go

--- skiplist_test.go
+++ skiplist_test.go
@@ -1,13 +1,12 @@
 package skiplist
 
 import (
-	"fmt"
 	"testing"
 )
 
 func Compare(a *SkipListNode, b *SkipListNode) bool {
-	value1 := a.Key.(string)
-	value2 := b.Key.(string)
+	value1 := a.Key.(int)
+	value2 := b.Key.(int)
 
 	if value1 > value2 {
 		return true
@@ -20,8 +19,8 @@ func TestSkipList(t *testing.T) {
 	skiplist := CreateSkipList(SKIPLIST_MAXLEVEL, nil)
 	skiplist.SetCompareFunc(Compare)
 
-	for i := 0; i < 100; i++ {
-		node := CreateSkipListNode(fmt.Sprintf("%d", i), i)
+	for i := 0; i < 1000000; i++ {
+		node := CreateSkipListNode(i, i)
 		skiplist.InsertNode(node)
 	}
 
